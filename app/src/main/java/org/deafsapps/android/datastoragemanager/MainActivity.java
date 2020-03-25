4a7b15d29abd4d55b1cd2e3fb3c84692a9415aa3
Pablo L. Sordo <pablodeafsapps@gmail.com>
2020-03-25 14:15:15 +0100

    Add initial design and funcionality

--- app/src/main/java/org/deafsapps/android/datastoragemanager/MainActivity.java
+++ app/src/main/java/org/deafsapps/android/datastoragemanager/MainActivity.java
@@ -3,13 +3,30 @@ package org.deafsapps.android.datastoragemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public final static int DEFAULT = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnOk = findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.btnOk) {
+            Log.i("btnOk", "'btnOk' clicked");
+        }
+
     }
 
 }
