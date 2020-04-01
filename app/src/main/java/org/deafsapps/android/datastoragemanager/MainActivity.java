0550a2ccc2d7eb8cb22700074d1b80ec7c89bb69
Pablo L. Sordo <pablodeafsapps@gmail.com>
2020-04-01 11:44:51 +0200

    Add 'External Storage' functionality

--- app/src/main/java/org/deafsapps/android/datastoragemanager/MainActivity.java
+++ app/src/main/java/org/deafsapps/android/datastoragemanager/MainActivity.java
@@ -1,9 +1,10 @@
 package org.deafsapps.android.datastoragemanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -20,6 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String UPDATE_INFO_KEY = "update-info";
     private static final String SHARED_PREFERENCES_FILENAME = "my-preferences-file";
     private static final String INTERNAL_STORAGE_FILENAME = "my-internal-file";
+    private static final String CUSTOM_DIRECTORY = "Pablo/my-folder";
+    private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 101;
     private static final String EMPTY_STRING = "";
 
     private EditText etInfo;
@@ -38,6 +47,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         final Button btnCurrentFiles = findViewById(R.id.btnGetFiles);
         btnCurrentFiles.setOnClickListener(this);
+
+        final Button btnExternalStorage = findViewById(R.id.btnExternalStorage);
+        btnExternalStorage.setOnClickListener(this);
 
         etInfo = findViewById(R.id.edtTextUpdate);
 
@@ -67,8 +79,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnGetFiles:
                 displayCurrentInternalFiles();
                 break;
+            case R.id.btnExternalStorage:
+                checkExternalStorageWritePermission();
+                break;
             default:
                 Log.w("switch", "No 'case' matched");
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                storeUpdateInfoIntoExternalStorage();
+            } else {
+                Log.w("requestPermission", "External storage usage not granted");
+                Toast.makeText(this, "External storage usage not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void checkExternalStorageWritePermission() {
+        final boolean isPermissionGranted =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+
+        if (isPermissionGranted) {
+            storeUpdateInfoIntoExternalStorage();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+        }
+    }
+
+    private void storeUpdateInfoIntoExternalStorage() {
+        final boolean isAvailable = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+
+        if (isAvailable) {
+            createDirectoryInDownloadsPublicFolder(CUSTOM_DIRECTORY);
+        } else {
+            Toast.makeText(this, "Location not available", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void createDirectoryInDownloadsPublicFolder(String path) {
+        final File personalFolder =
+                new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), path);
+
+        if (!personalFolder.mkdirs()) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, ":)", Toast.LENGTH_SHORT).show();
         }
 
     }
